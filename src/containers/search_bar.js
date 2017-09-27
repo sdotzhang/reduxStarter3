c6759dd17f1f5152f507c55c3c47b2e937e6fcc0
Shaonan Zhang <sdotzhang@gmail.com>
2017-09-27 09:04:26 -0700

    get started with middleware
    Change-Id: I6ad8bf38fc037bbf4a21ab12791e3abe8cf70db7

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { term: '' };
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     render () {
         return (
-            <form className="input-group">
-                <input />
+            <form className="input-group" onSubmit={this.onFormSubmit}>
+                <input
+                    className="form-control"
+                    placeholder="Get a five-day forcast in your favorite cities"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">
                         Submit
