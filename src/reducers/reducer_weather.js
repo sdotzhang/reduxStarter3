5273d2683401c2ff314e4f426c37f8fbe5996319
Shaonan Zhang <sdotzhang@gmail.com>
2017-10-11 09:43:51 -0700

    update reducer to take fetched data
    Change-Id: I3f1698e793b0a65164cbbb757d4a27e01cd07407

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,12 @@
-export default function(state = null, action) {
+import { FEATCH_WEATHER } from '../actions/index'
+
+export default function(state = [], action) {
+    switch (action.type) {
+        case FEATCH_WEATHER:
+            return [ action.payload.data, ...state ];
+
+        default:
+            break;
+    }
     return state;
 }
