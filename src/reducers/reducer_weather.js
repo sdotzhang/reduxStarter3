ed7753c361e208d44de75aed761e28fbf7448b3e
Shaonan Zhang <sdotzhang@gmail.com>
2017-10-15 20:43:29 -0700

    added google map integration
    Change-Id: Ie0c0b0be8251748510422a3bdcab948ff4fbfc1b

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,12 +1,10 @@
-import { FEATCH_WEATHER } from '../actions/index'
+import { FETCH_WEATHER } from '../actions/index'
 
 export default function(state = [], action) {
     switch (action.type) {
-        case FEATCH_WEATHER:
+        case FETCH_WEATHER:
             return [ action.payload.data, ...state ];
-
         default:
-            break;
+            return state
     }
-    return state;
 }
